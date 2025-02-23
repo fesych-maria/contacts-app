ea6485727b255bc3a2222d39a2563c1d38af7c3e
maria <fesychmarimia@gmail.com>
2025-02-23 15:48:51 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
+import "./js/registration.js";
+import "./js/login.js";
+import { refs } from "./js/refs.js";
 
+const token = localStorage.getItem(refs.LS_KEY);
+if (token) {
+  location.replace("/contacts.html");
+}
